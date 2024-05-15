16aa8473e99fb1313beb73c5c370d15d55180297
Liudmila <149329092+anophesper@users.noreply.github.com>
2024-05-15 16:27:22 +0300

    Interfase IBranchAdmin

---
+++ ExpressPost/ExpressPost/Classes/IBranchAdmin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressPost.Classes
+{
+    internal interface IBranchAdmin
+    {
+        void MarkArrival(Parcel parcel);
+        void MarkDeparture(Parcel parcel);
+    }
+}
